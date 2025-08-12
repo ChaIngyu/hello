f196e91a8842b1c249d41cd83b3e652459ce4b40
ChaIngyu <55075795+ChaIngyu@users.noreply.github.com>
2025-08-12 14:20:00 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -2,8 +2,16 @@ import requests
 import json
 from datetime import datetime
 from zoneinfo import ZoneInfo
+import os
 
 now = datetime.now(ZoneInfo("Asia/Seoul"))
 current_datetime = now.strftime("%Y-%m-%d %H:%M")
 print(current_datetime)
 
+webhook_url = os.getenv("WEBHOOK_LINK")
+
+message = {
+    "text": current_datetime
+}
+
+requests.post(webhook_url, data=json.dumps(message))
