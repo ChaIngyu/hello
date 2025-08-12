83f775ba5aa156c5f0e0e56280909eb4ac046f22
ChaIngyu <55075795+ChaIngyu@users.noreply.github.com>
2025-08-12 18:30:56 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -1,17 +1,47 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 import os
+import gspread
+from google.oauth2.service_account import Credentials
+
 
 now = datetime.now(ZoneInfo("Asia/Seoul"))
-current_datetime = now.strftime("%Y-%m-%d %H:%M")
+current_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
 print(current_datetime)
+
 
 webhook_url = os.getenv("WEBHOOK_LINK")
 
-message = {
-    "text": current_datetime
-}
+service_account_info = json.loads(os.environ['GOOGLE_SERVICE_ACCOUNT_KEY'])
+credentials = Credentials.from_service_account_info(
+    service_account_info,
+    scopes=["https://www.googleapis.com/auth/spreadsheets"]
+)
+gc = gspread.authorize(credentials)
 
-# requests.post(webhook_url, data=json.dumps(message))
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/116J72CQO0K71rcUU3_qT4RE9mpykDXwKYQ9Eb83qN8A/edit?gid=0#gid=0"
+doc = gc.open_by_url(spreadsheet_url)
+
+worksheet = doc.worksheet("시트1")
+
+all_rows = worksheet.get_values()[1:]
+
+for i in len(all_rows):
+    title, message, start_time, period = all_rows[i]
+    start_dt = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    end_dt = start_dt + timedelta(minutes=int(period))
+    if(start_dt < now and now < end_dt):
+        webhook_message = {
+            "text": f"{title} : {message}"
+        }
+        requests.post(webhook_url, data=json.dumps(webhook_message))
+    elif(end_dt <= now):
+        worksheet.delete_rows(i+2)
+
+
+
+
+
+
