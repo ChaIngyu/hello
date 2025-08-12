0ed600eed7808ea8ceffcd8466882a6105827a2a
ChaIngyu <55075795+ChaIngyu@users.noreply.github.com>
2025-08-12 14:11:41 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -7,10 +7,3 @@ now = datetime.now(ZoneInfo("Asia/Seoul"))
 current_datetime = now.strftime("%Y-%m-%d %H:%M")
 print(current_datetime)
 
-webhook_url = 'https://hooks.slack.com/services/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'
-
-message = {
-    "text": current_datetime
-}
-
-requests.post(webhook_url, data=json.dumps(message))
